44f5552cf2466d08b06415cedbb50b5af59c4eb3
Elyes-Ferjani <officialef@hotmail.com>
2021-01-22 22:20:55 +0100

    RequestHandler Added

---
+++ utilis/RequestHandler.js
@@ -0,0 +1,54 @@
+const _ = require('lodash');
+
+class RequestHandler{
+    constructor(logger){
+        this.logger = logger
+    }
+
+    throwIf(fn, status, errorType, errorMessage) {
+		return result => (fn(result) ? this.throwError(status, errorType, errorMessage)() : result);
+    }
+    
+    throwError(status, errorType, errorMessage) {
+		return (e) => {
+			if (!e) e = new Error(errorMessage || 'Default Error');
+			e.status = status;
+			e.errorType = errorType;
+			throw e;
+		};
+	}
+
+    catchError(res,error){
+        if(!error) error = new Error('Default Error');
+        res.status(error.status || 500).json({
+            type: 'error',
+            message: error.message || 'Unhandled error'
+        })
+    }
+
+    sendSuccess(res,message,status){
+        this.logger.log(`A request has been made and proccessed successfully at ${new Date()}`, `info`);
+        return (data, globalData)=>{
+            if(_.isUndefined(status)){
+                status = 200
+            }
+            res.status(status).json({
+                type: 'success',
+                message: message || 'Success result', 
+                data, 
+                ...globalData,
+            })
+        }
+    }
+
+    sendError(req,res,error){
+        this.logger.log(`error, error during processing request: ${req.protocol}://${req.get('host')}${req.originalUrl} , message details: ${error.message}`, 'error')
+        return res.status(error.status || 500).json({
+            type: 'error',
+            message: error.message || 'Unhandled error',
+            error,
+        })
+    }
+}
+
+module.export = RequestHandler
