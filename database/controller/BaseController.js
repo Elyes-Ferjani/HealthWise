19687318b84809e26ddd501ccf6ca5703be1b063
Elyes-Ferjani <officialef@hotmail.com>
2021-03-16 21:05:37 +0100

    Added BodyParts Organs Controller

--- database/controller/BaseController.js
+++ database/controller/BaseController.js
@@ -7,21 +7,34 @@ const Body = require('../models/body')(sequelize)
 const logger = new Logger();
 const errorHandler = new RequestHandler(logger);
 
-class BaseController{
-    constructor(options){
+class BaseController {
+    constructor(options) {
         this.limit = 20;
         this.options = options
     }
 
-    static async getBody(req,modelName){
+    static async getBody(req, modelName) {
         let result;
-        try{
-            // need to check with modelName if db
-                result = await req.app.get('db')[modelName].findAll().catch(()=>{ 
-                errorHandler.throwIf(r=> !r, 400, 'not found', 'Resource not found'),
-                errorHandler.throwError(500, 'sequelize error ,some thing wrong with either the data base connection or schema')
-                })
-        } catch (error){
+        try {
+            result = await req.app.get('db')[modelName].findAll().catch(() => {
+                errorHandler.throwIf(r => !r, 400, 'not found', 'Resource not found'),
+                    errorHandler.throwError(500, 'sequelize error ,some thing wrong with either the database connection or schema')
+            })
+        } catch (error) {
+            return Promise.reject(error)
+        }
+        return result
+    }
+
+    static async getBodyParts(req, modelName) {
+        let result;
+        const id = req.params.partId
+        try {
+            result = await req.app.get('db')[modelName].findAll({where: {body_id: id}}).catch(() => {
+                errorHandler.throwIf(r => !r, 400, 'not found', 'Resource not found'),
+                    errorHandler.throwError(500, 'sequelize error, some thing wrong with either the database connection or schema')
+            })
+        } catch (error) {
             return Promise.reject(error)
         }
         return result
--- database/controller/BodyController.js
+++ database/controller/BodyController.js
@@ -8,12 +8,21 @@ const requestHandler = new RequestHandler(logger)
 
 class BodyParts extends BaseController {
 
-    static async getBody(req,res) {
+    static async getBody(req, res) {
         try {
             const result = await super.getBody(req, 'body');
             return requestHandler.sendSuccess(res, 'Main BodyPart Data Extracted')({ result })
-        } catch(error){
-            return requestHandler.sendError(req,res,error)
+        } catch (error) {
+            return requestHandler.sendError(req, res, error)
+        }
+    }
+
+    static async getBodyParts(req, res) {
+        try {
+            const result = await super.getBodyParts(req, 'bodypart');
+            return requestHandler.sendSuccess(res, 'Internal BodyPart Data Extracted')({ result })
+        } catch (error) {
+            return requestHandler.sendError(req, res, error)
         }
     }
 }
