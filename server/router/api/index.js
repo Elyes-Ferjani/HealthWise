81fb43820951d88861f451c9ffd1c6411c27a485
Elyes-Ferjani <officialef@hotmail.com>
2021-08-06 22:16:02 +0200

    Adding Routes

--- server/router/api/index.js
+++ server/router/api/index.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 router.use('/body', require('./bodyRouter'))
 router.use('/body', require('./bodyPartRouter'))
 router.use('/body', require('./commonSymptomsRouter'))
+router.use('/body', require('./causesRouter'))
+router.use('/body', require('./suspectRouter'))
 
 module.exports = router
